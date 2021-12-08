bd59f4da3405fe3f597162ce51fab9466dc10294
shumman.sue <shumnan.sue@ascendcorp.com>
2021-12-08 11:20:45 +0700

    Configure cypress-regression-test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,12 +1,15 @@
 const cucumber = require("cypress-cucumber-preprocessor").default;
 const browserify = require("@cypress/browserify-preprocessor");
+const getCompareSnapshotsPlugin = require('cypress-visual-regression/dist/plugin');
 
-module.exports = (on) => {
+module.exports = (on, config) => {
+    // Configure from cypress-cucumber-preprocessor
     const options = browserify.defaultOptions;
-
     options.browserifyOptions.plugin.unshift(['tsify']);
     // Or, if you need a custom tsconfig.json for your test files:
     // options.browserifyOptions.plugin.unshift(['tsify', {project: 'path/to/other/tsconfig.json'}]);
-
     on("file:preprocessor", cucumber(options));
+
+    // Configure from cypress-visual-regression
+    getCompareSnapshotsPlugin(on, config);
 };
---
+++ cypress/support/index.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+interface CompareSnapshotOptions {
+    errorThreshold: number;
+}
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            compareSnapshot(name: string): void;
+            compareSnapshot(name: string, errorThreshold?: number): void;
+            compareSnapshot(name: string, options?: Partial<Cypress.ScreenshotOptions | CompareSnapshotOptions> ): void;
+        }
+    }
+}
+
+export default function compareSnapshotCommand(options?: Partial<Cypress.ScreenshotOptions | CompareSnapshotOptions>): void;
+
